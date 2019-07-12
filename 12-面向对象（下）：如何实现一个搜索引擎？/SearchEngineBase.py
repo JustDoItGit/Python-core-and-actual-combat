623434e6f7ee4cb469e6b1cd49669a36f89416e3
cool <cool_kj@126.com>
2019-07-12 23:57:56 +0800

    SearchEngineBase类

---
+++ 12-面向对象（下）：如何实现一个搜索引擎？/SearchEngineBase.py
@@ -0,0 +1,46 @@
+class SearchEngineBase(object):
+    def __init__(self):
+        pass
+
+    def add_corpus(self, file_path):
+        with open(file_path, 'r') as fin:
+            text = fin.read()
+        self.process_corpus(file_path, text)
+
+    def process_corpus(self, id, text):
+        raise Exception('process_corpus not implemented')
+
+    def search(self, query):
+        raise Exception('search not implemented')
+
+
+def main(search_engine):
+    for file_path in ['1.txt', '2.txt', '3.txt', '4.txt', '5.txt']:
+        search_engine.add_corpus(file_path)
+
+    while True:
+        query = input('请输入要搜索的关键词：')
+        results = search_engine.search(query)
+        print('found {} result(s):'.format(len(results)))
+        for result in results:
+            print(result)
+
+
+class SimpleEngine(SearchEngineBase):
+    def __init__(self):
+        super(SimpleEngine, self).__init__()
+        self.__id_to_texts = {}
+
+    def process_corpus(self, id, text):
+        self.__id_to_texts[id] = text
+
+    def search(self, query):
+        results = []
+        for id, text in self.__id_to_texts.items():
+            if query in text:
+                results.append(id)
+        return results
+
+
+search_engine = SimpleEngine()
+main(search_engine)
