4b11c6539a2e4bc25e5817b7af8c479c3cb32d7b
cool <cool_kj@126.com>
2019-07-13 15:34:12 +0800

    bag of words

---
+++ 12-面向对象（下）：如何实现一个搜索引擎？/Bag_of_Words.py
@@ -0,0 +1,45 @@
+import re
+from SearchEngineBase import main
+from SearchEngineBase import SearchEngineBase
+
+
+class BOWEngine(SearchEngineBase):
+    def __init__(self):
+        super(BOWEngine, self).__init__()
+        self.__id_to_words = {}
+
+    def process_corpus(self, id, text):
+        self.__id_to_words[id] = self.parse_text_to_words(text)
+
+    def search(self, query):
+        query_words = self.parse_text_to_words(query)
+        results = []
+        for id, words in self.__id_to_words.items():
+            if self.query_match(query_words, words):
+                results.append(id)
+        return results
+
+    @staticmethod
+    def query_match(query_words, words):
+        for query_word in query_words:
+            if query_word not in words:
+                return False
+        return True
+
+    @staticmethod
+    def parse_text_to_words(text):
+        # 使用正则表达式去除标点符号和换行符，用空格代替
+        text = re.sub(r'[^\w]', ' ', text)
+        # 转为小写
+        text = text.lower()
+        # 生成所有单词的列表
+        word_list = text.split(' ')
+        # 去除空白单词
+        word_list = filter(None, word_list)
+        # 返回单词的 set
+        return set(word_list)
+
+
+if __name__ == '__main__':
+    search_engine = BOWEngine()
+    main(search_engine)
