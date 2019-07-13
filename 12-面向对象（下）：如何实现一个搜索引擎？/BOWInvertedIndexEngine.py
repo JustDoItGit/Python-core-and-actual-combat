ae2e641888ef279603559547aaa41faa1cc23e20
cool <cool_kj@126.com>
2019-07-13 16:27:05 +0800

    BOWIinvertedIindexEngine

---
+++ 12-面向对象（下）：如何实现一个搜索引擎？/BOWInvertedIndexEngine.py
@@ -0,0 +1,67 @@
+import re
+from SearchEngineBase import main
+from SearchEngineBase import SearchEngineBase
+
+
+class BOWIinvertedIindexEngine(SearchEngineBase):
+    def __init__(self):
+        super(BOWIinvertedIindexEngine, self).__init__()
+        self.inverted_index = {}
+
+    def process_corpus(self, id, text):
+        words = self.parse_text_to_words(text)
+        for word in words:
+            if word not in self.inverted_index:
+                self.inverted_index[word] = []
+            self.inverted_index[word].append(id)
+
+    def search(self, query):
+        query_words = list(self.parse_text_to_words(query))
+        query_words_index = []
+        for query_word in query_words:
+            query_words_index.append(0)
+
+        # 这后面的倒排索引暂时不理解没关系，这里重点理解类的继承，和方法重写就行
+        # 如果某一个查询单词的倒序索引为空，我们就立刻返回
+        for query_word in query_words:
+            if query_word not in self.inverted_index:
+                return []
+
+        result = []
+        while True:
+            # 首先，获得当前状态下所有倒序索引的 index
+            current_ids = []
+            for idx, query_word in enumerate(query_words):
+                current_index = query_words_index[idx]
+                current_inverted_list = self.inverted_index[query_word]
+                # 已经遍历到了某一个倒序索引的末尾，结束 search
+                if current_index >= len(current_inverted_list):
+                    return result
+                current_ids.append(current_inverted_list[current_index])
+            # 然后，如果 current_ids 的所有元素都一样，那么表明这个单词在这个元素对应的文档中都出现了
+            if all(x == current_ids[0] for x in current_ids):
+                result.append(current_ids[0])
+                query_words_index = [x + 1 for x in query_words_index]
+                continue
+            # 如果不是，我们就把最小的元素加一
+            min_val = min(current_ids)
+            min_val_pos = current_ids.index(min_val)
+            query_words_index[min_val_pos] += 1
+
+    @staticmethod
+    def parse_text_to_words(text):
+        # 使用正则表达式去除标点符号和换行符，用空格代替
+        text = re.sub(r'[^\w]', ' ', text)
+        # 转为小写
+        text = text.lower()
+        # 生成所有单词的列表
+        word_list = text.split(' ')
+        # 去除空白单词
+        word_list = filter(None, word_list)
+        # 返回单词的 set
+        return set(word_list)
+
+
+if __name__ == '__main__':
+    search_engine = BOWIinvertedIindexEngine()
+    main(search_engine)
