c8b366bd3de8948ba27e44360397d0df9a8782d6
cool <cool_kj@126.com>
2019-07-15 16:20:50 +0800

    wrapper 和 被装饰函数的参数一样

--- 17-强大的装饰器/带有不确定参数的装饰器.py
+++ 17-强大的装饰器/带有不确定参数的装饰器.py
@@ -1,6 +1,7 @@
 def my_decorator(func):
     def wrapper(*args, **kwargs):
         print('wrapper of decorator')
+        print('wrapper {}'.format(args[0]))
         func(*args, **kwargs)
 
     return wrapper
