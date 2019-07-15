2eb69b67b6f38bbf197db39cc376fa248018f1d5
cool <cool_kj@126.com>
2019-07-15 15:49:29 +0800

    类装饰器的嵌套

---
+++ 17-强大的装饰器/装饰器的嵌套.py
@@ -0,0 +1,29 @@
+import functools
+
+
+def my_decorator1(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        print('wrapper of decorator1')
+        func(*args, **kwargs)
+
+    return wrapper
+
+
+def my_decorator2(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        print('wrapper of decorator2')
+        func(*args, **kwargs)
+
+    return wrapper
+
+
+@my_decorator1
+@my_decorator2
+@my_decorator1
+def greet(message):
+    print(message)
+
+
+greet('hello world')
