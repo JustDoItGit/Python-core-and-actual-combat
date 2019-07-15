54207b7a9548bf130a03c116f38abcbfed33703e
cool <cool_kj@126.com>
2019-07-15 15:29:59 +0800

    装饰器保留被装饰函数元信息

---
+++ 17-强大的装饰器/保留原函数的元信息.py
@@ -0,0 +1,20 @@
+import functools
+
+
+def my_decorator(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        print('wrapper of decorator')
+        func(*args, **kwargs)
+
+    return wrapper
+
+
+@my_decorator
+def greet(message):
+    print(message)
+
+
+greet('hello world')
+print(greet.__name__)
+help(greet)
