6df627a849cc7cfcfcdc15f26e0be5d19005f45a
cool <cool_kj@126.com>
2019-07-15 15:23:12 +0800

    带有自定义参数的装饰器

---
+++ 17-强大的装饰器/带有自定义参数的装饰器.py
@@ -0,0 +1,18 @@
+def repeat(num):
+    def my_decorator(func):
+        def wrapper(*args, **kwargs):
+            for i in range(num):
+                print('wrapper of decorator')
+                func(*args, **kwargs)
+
+        return wrapper
+
+    return my_decorator
+
+
+@repeat(4)
+def greet(message):
+    print(message)
+
+
+greet('hello world')
