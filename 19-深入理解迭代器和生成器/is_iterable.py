7cdc50fddc3de77a2676f02422187bff9f4c2754
cool <cool_kj@126.com>
2019-07-16 23:57:22 +0800

    iterable

---
+++ 19-深入理解迭代器和生成器/is_iterable.py
@@ -0,0 +1,19 @@
+def is_iterable(param):
+    try:
+        iter(param)
+        return True
+    except TypeError:
+        return False
+
+
+params = [
+    1234,
+    '1234',
+    [1, 2, 3, 4],
+    set([1, 2, 3, 4]),
+    {1: 1, 2: 2, 3: 3, 4: 4},
+    (1, 2, 3, 4)
+]
+
+for param in params:
+    print('{} is iterable? {}'.format(param, is_iterable(param)))
