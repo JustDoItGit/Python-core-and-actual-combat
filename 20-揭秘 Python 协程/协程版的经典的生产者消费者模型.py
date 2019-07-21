090dad5aeaf20c424df308ff9c06c896bd2635b0
cool <cool_kj@126.com>
2019-07-21 23:53:57 +0800

    协程版的经典的生产者消费者模型

---
+++ 20-揭秘 Python 协程/协程版的经典的生产者消费者模型.py
@@ -0,0 +1,40 @@
+import time
+import random
+import asyncio
+
+
+async def consumer(queue, id):
+    while True:
+        val = await queue.get()
+        print('{} get a val: {}'.format(id, val))
+        await asyncio.sleep(1)
+
+
+async def producer(queue, id):
+    for i in range(5):
+        val = random.randint(1, 10)
+        await queue.put(val)
+        print('{} put a val: {}'.format(id, val))
+        await asyncio.sleep(1)
+
+
+async def main():
+    queue = asyncio.Queue()
+
+    consumer_1 = asyncio.create_task(consumer(queue, 'consumer_1'))
+    consumer_2 = asyncio.create_task(consumer(queue, 'consumer_2'))
+
+    producer_1 = asyncio.create_task(producer(queue, 'producer_1'))
+    producer_2 = asyncio.create_task(producer(queue, 'producer_2'))
+
+    await asyncio.sleep(10)
+    consumer_1.cancel()
+    consumer_2.cancel()
+
+    await asyncio.gather(consumer_1, consumer_2, producer_1, producer_2, return_exceptions=True)
+
+
+start = time.perf_counter()
+asyncio.run(main())
+end = time.perf_counter()
+print('cost time: {} s'.format(end - start))
