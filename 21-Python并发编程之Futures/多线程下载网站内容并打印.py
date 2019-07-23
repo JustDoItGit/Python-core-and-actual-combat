9a40e5477bc302fb20a1c966d3e53b76aead6c57
cool <cool_kj@126.com>
2019-07-23 11:24:10 +0800

    多线程下载网站内容并打印

---
+++ 21-Python并发编程之Futures/多线程下载网站内容并打印.py
@@ -0,0 +1,42 @@
+import time
+import requests
+import threading
+import concurrent.futures
+
+
+def download_one(url):
+    resp = requests.get(url)
+    print('Read {} from {}'.format(len(resp.content), url))
+
+
+def download_all(sites):
+    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+        executor.map(download_one, sites)
+
+
+def main():
+    sites = [
+        'https://en.wikipedia.org/wiki/Portal:Arts',
+        'https://en.wikipedia.org/wiki/Portal:History',
+        'https://en.wikipedia.org/wiki/Portal:Society',
+        'https://en.wikipedia.org/wiki/Portal:Biography',
+        'https://en.wikipedia.org/wiki/Portal:Mathematics',
+        'https://en.wikipedia.org/wiki/Portal:Technology',
+        'https://en.wikipedia.org/wiki/Portal:Geography',
+        'https://en.wikipedia.org/wiki/Portal:Science',
+        'https://en.wikipedia.org/wiki/Computer_science',
+        'https://en.wikipedia.org/wiki/Python_(programming_language)',
+        'https://en.wikipedia.org/wiki/Java_(programming_language)',
+        'https://en.wikipedia.org/wiki/PHP',
+        'https://en.wikipedia.org/wiki/Node.js',
+        'https://en.wikipedia.org/wiki/The_C_Programming_Language',
+        'https://en.wikipedia.org/wiki/Go_(programming_language)'
+    ]
+    start_time = time.perf_counter()
+    download_all(sites)
+    end_time = time.perf_counter()
+    print('Download {} sites in {} seconds'.format(len(sites), end_time - start_time))
+
+
+if __name__ == '__main__':
+    main()
