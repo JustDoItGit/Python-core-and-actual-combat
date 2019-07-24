c2b1e74b1b939d79b909c523764fcd4d5fb327f6
cool <cool_kj@126.com>
2019-07-24 15:21:45 +0800

    循环引用

---
+++ 24-带你解析 Python 垃圾回收机制/循环引用.py
@@ -0,0 +1,26 @@
+import os
+import sys
+import psutil
+
+
+# 显示当前 Python 程序占用内存大小
+def show_memory_info(hint):
+    pid = os.getpid()
+    p = psutil.Process(pid)
+
+    info = p.memory_full_info()
+    memory = info.uss / 1024. / 1024
+    print('{} memory used: {} MB'.format(hint, memory))
+
+
+def func():
+    show_memory_info('initial')
+    a = [i for i in range(10000000)]
+    b = [i for i in range(10000000)]
+    show_memory_info('after a created')
+    a.append(b)
+    b.append(a)
+
+
+func()
+show_memory_info('finished')
