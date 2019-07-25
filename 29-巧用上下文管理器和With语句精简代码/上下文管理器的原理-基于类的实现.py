70c3c087faa47f8107c480268de4a5da626384f3
cool <cool_kj@126.com>
2019-07-25 10:18:40 +0800

    上下文管理器的原理-基于类的实现

---
+++ 29-巧用上下文管理器和With语句精简代码/上下文管理器的原理-基于类的实现.py
@@ -0,0 +1,21 @@
+class FileManager:
+    def __init__(self, name, mode):
+        print('calling __init__ method')
+        self.name = name
+        self.mode = mode
+        self.file = None
+
+    def __enter__(self):
+        print('calling __enter__ name')
+        self.file = open(self.name, self.mode)
+        return self.file
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        print('calling __exit__ method')
+        if self.file:
+            self.file.close()
+
+
+with FileManager('text.txt', 'w') as f:
+    print('ready to write to file')
+    f.write('hello world')
