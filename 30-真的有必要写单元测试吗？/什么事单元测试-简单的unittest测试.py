3437a5013a3ea27475b2bb5c5a01815750664e05
cool <cool_kj@126.com>
2019-07-25 13:40:43 +0800

    什么事单元测试-简单的unittest测试

---
+++ 30-真的有必要写单元测试吗？/什么事单元测试-简单的unittest测试.py
@@ -0,0 +1,26 @@
+import unittest
+
+
+# 将要被测试的排序函数
+def sort(arr):
+    l = len(arr)
+    for i in range(0, l):
+        for j in range(i + 1, l):
+            if arr[i] >= arr[j]:
+                arr[i], arr[j] = arr[j], arr[i]
+
+
+# 编写子类继承 unittest.TestCase
+class TestSort(unittest.TestCase):
+    # 以 test 开头的函数将会被测试
+    def test_sort(self):
+        arr = [3, 4, 1, 5, 6]
+        sort(arr)
+        # assert 结果跟我们期待一样
+        self.assertEqual(arr, [1, 3, 4, 5, 6])
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+    unittest.main(argv=['first-arg-is-ignored'], exit=False)
