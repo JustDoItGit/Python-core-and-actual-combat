a38a056606331249570a3901aa0b310a21eb7195
cool <cool_kj@126.com>
2019-07-25 22:10:32 +0800

    改善fib函数效率

---
+++ 31-pdb & cProfile：调试和性能分析的法宝/改善fib函数效率.py
@@ -0,0 +1,33 @@
+import cProfile
+
+
+def memoize(f):
+    memo = {}
+
+    def helper(x):
+        if x not in memo:
+            memo[x] = f(x)
+        return memo[x]
+
+    return helper
+
+
+@memoize
+def fib(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib(n - 1) + fib(n - 2)
+
+
+def fib_seq(n):
+    res = []
+    if n > 0:
+        res.extend(fib_seq(n - 1))
+    res.append(fib(n))
+    return res
+
+
+cProfile.run('fib_seq(30)')
